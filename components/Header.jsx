9caf74cf099a9e3b8f83b6366491b50f587e89ac
Wen Qi <wen15th@outlook.com>
2025-06-30 22:45:27 -0400

    update Header and Desktop Navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import Nav from "@/components/Nav";
+
+// components
+import Nav from "./Nav";
 
 const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-            <div className="container mx-auto">
+            <div className="container mx-auto flex justify-between items-center">
                 {/* logo */}
                 <Link href="/">
                     <h1 className="text-4xl font-semibold">
                         Wen <span className="text-accent">.</span>
                     </h1>
                 </Link>
-                {/* desktop nav*/}
-                <Nav />
+
+                {/* desktop nav & hire me button */}
+                {/* hidden is default here */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* mobile nav */}
+                <div className="xl:hidden">mobile nav</div>
+
             </div>
         </header>
     );
