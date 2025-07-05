d211b91dbf59f67bbac3ef598bc924fe85b82b58
Wen Qi <wen15th@outlook.com>
2025-07-05 17:19:41 -0400

    Mobile Nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,11 @@ import { Button } from "./ui/button";
 
 // components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
-        <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+        <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 {/* logo */}
                 <Link href="/">
@@ -25,7 +26,9 @@ const Header = () => {
                 </div>
 
                 {/* mobile nav */}
-                <div className="xl:hidden">mobile nav</div>
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
 
             </div>
         </header>
